b15d11b8bc4b74a1dfd49bf4e88a665c8bbf71fb
Alex Gurr <alexgurr@outlook.com>
2021-03-28 11:28:28 +1100

    Update botty URL

--- chatter/src/config.js
+++ chatter/src/config.js
@@ -1,5 +1,5 @@
 export default {
-  BOT_SERVER_ENDPOINT: 'https://botty-beep-boop.herokuapp.com',
+  BOT_SERVER_ENDPOINT: 'https://botty.alexgurr.com',
   SEND_AUDIO_URL: 'https://puu.sh/GSHJ0/25fae22f76.mp3',
   RECEIVE_AUDIO_URL: 'https://puu.sh/GSHIU/df806a9cb8.mp3'
 };
