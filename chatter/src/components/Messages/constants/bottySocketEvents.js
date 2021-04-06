6e8fed0c233486e410c6cd8e60295c5a5aea97a1
vpogonea <vasile.pogonea@endava.com>
2021-04-06 04:10:05 +0300

    feat: add user types and botty events

---
+++ chatter/src/components/Messages/constants/bottySocketEvents.js
@@ -0,0 +1,5 @@
+export default {
+  BOT_TYPING: 'bot-typing',
+  BOT_MESSAGE: 'bot-message',
+  USER_MESSAGE: 'user-message',
+};
---
+++ chatter/src/components/Messages/constants/userTypes.js
@@ -0,0 +1,4 @@
+export default {
+    ME: 'me',
+    BOT: 'bot',
+};
